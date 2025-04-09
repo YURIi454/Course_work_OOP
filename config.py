3379fb870395a1f0c27b4f7bbcce77d5a8fc6579
Yurii <yurii.1986.e@gmail.com>
2025-04-09 09:57:47 +0400

    Create project.

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+ROOT_DIR = os.path.dirname(__file__)
+LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+PATH_JSON =''
+PATH_CSV = ''
+PATH_XLSX = ''
+PATH_LOGS = os.path.join(LOGS_DIR, 'logs.log')
